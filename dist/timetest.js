d022c0d53905e6699a365d3f17fd513d1bea7219
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2023-01-13 12:02:12 -0300

    railway test 2

--- dist/timetest.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const date_fns_1 = require("date-fns");
-const today = (0, date_fns_1.startOfDay)(new Date('2023-01-14T11:06:17.267Z'));
-const tomorrow = (0, date_fns_1.endOfDay)(new Date('2023-01-13T11:06:17.267Z'));
-console.log(today);
